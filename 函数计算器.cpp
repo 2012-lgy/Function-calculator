05fcfe8d82130075f11d3064e94f969c43bc28ad
Danmit <3388964469@qq.com>
2024-09-16 12:35:14 +0800

    Add files via upload

--- 函数计算器.cpp
+++ 函数计算器.cpp
@@ -3,11 +3,55 @@
 using namespace std;
 const long double rtd=57.295779513082320876798154814105170332405;
 const long double e=2.7182818284590452353602874713526624977572;
-unsigned short stp=10;
+unsigned short stp=10,lang=1,forgc=15,backc=0;
 typedef unsigned long long ull;
 void SetColor(int ForgC, int BackC) {
     WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
+}
+void err(){
+	SetColor(4,0);
+	switch(lang){
+		case 1:
+			cout<<"[错误]";
+			break;
+		case 2:
+			cout<<"[Error]";
+			break;
+	}
+}
+void prom(){
+	SetColor(1,0);
+	switch(lang){
+		case 1:
+			cout<<"[提示]";
+			break;
+		case 2:
+			cout<<"[Prompt]";
+			break;
+	}
+}
+void warn(){
+	SetColor(6,0);
+	switch(lang){
+		case 1:
+			cout<<"[警告]";
+			break;
+		case 2:
+			cout<<"[Warning]";
+			break;
+	}
+}
+void succ(){
+	SetColor(2,0);
+	switch(lang){
+		case 1:
+			cout<<"[成功运行]";
+			break;
+		case 2:
+			cout<<"[Successful completion]";
+			break;
+	}
 }
 bool ispn(ull n){
 	if(n<2) return 0;
@@ -28,10 +72,17 @@ ull max(ull i,ull n){
 	else return i;
 }
 void pf(ull num){
-	ull cn,np=2,ny=0,y[1000],errcnt;
+	ull cn,np=2,ny=0,y[1000];
 	if(num<2){
-		SetColor(4,0);
-		cout<<"错误，值<2！\n";
+		err();
+		switch(lang){
+			case 1:
+				cout<<"值<2！\n";
+				break;
+			case 2:
+				cout<<"Value<2！\n";
+				break;
+		}
 	}else{
 		cn=num;
 		while(iscn(num)){
@@ -46,7 +97,17 @@ void pf(ull num){
 				}
 			}
 		}
-		if(ispn(cn)) cout<<cn<<"是质数。"; else{
+		succ();
+		if(ispn(cn)){
+			switch(lang){
+			case 1:
+				cout<<cn<<"是质数。\n"; 
+				break;
+			case 2:
+				cout<<" is prime number\n";
+				break;
+			}
+		}else{
 			if(num!=1) y[ny]=num;
 			cout<<cn<<" = ";
 			for(int i=0;i<ny;i++) cout<<y[i]<<"×";
@@ -55,7 +116,7 @@ void pf(ull num){
 		cout<<endl;
 	}
 }
-unsigned long long gcd(unsigned long long n,unsigned long long x){
+ull gcd(ull n,ull x){
 	if(n>x) swap(n,x);
 	if(ispn(n)&&ispn(x)) return 1; else if(x%n==0) return n; else if(n==1) return 1; else{
 		while(1){
@@ -75,10 +136,33 @@ long double mod(long double n,long double m){
 }
 void cb(){
 	long double n,num=0;
-	ull lb,b;
-	cin>>n>>lb>>b;
+	long long lb,b;
+	cin>>n>>lb;
+	if(lb>10||lb<1) warn();
+	switch(lang){
+		case 1:
+			if(lb>10) cout<<"值>10，已将值转为10。\n";
+			if(lb<1) cout<<"值<1，已将值转为1。\n";
+			break;
+		case 2:
+			if(lb>10) cout<<"Value>10，The value has been converted to 10.\n";
+			if(lb<1) cout<<"Value<1，The value has been converted to 1.\n";
+			break;
+	}
+	lb=max(min(lb,10),1);
+	cin>>b;
+	if(b>36||b<2) warn();
+	switch(lang){
+		case 1:
+			if(b>36) cout<<"值>36，已将值转为36。\n";
+			if(b<2) cout<<"值<2，已将值转为2。\n";
+			break;
+		case 2:
+			if(b>36) cout<<"Value>36，The value has been converted to 36.\n";
+			if(b<2) cout<<"Value<2，The value has been converted to 2.\n";
+			break;
+	}
 	b=max(min(b,36),2);
-	lb=max(min(lb,10),2);
 	int zn=n;
 	long double xn=n-zn;
 	char zdig[100]={0},xdig[1000]={0};
@@ -100,7 +184,9 @@ void cb(){
 		znum/=b;
 		di=i;
 		if(znum==0) break;
-	}for(int i=di;i>-1;i--) cout<<zdig[i];
+	}
+	succ();
+	for(int i=di;i>-1;i--) cout<<zdig[i];
 	if(xnum!=0){
 		cout<<'.';
 		for(int i=0;i<1000;i++){
@@ -118,244 +204,302 @@ void cb(){
 }
 int main(){
 	string code;
-	cout<<"函数计算器 v1.7.0\n1998291 B\n"<<setprecision(10);
+	cout<<"函数计算器 v1.8.0\n2005647 B\n"<<setprecision(10);
 	while(1){
 		cin>>code;
 		if(code=="pf"){
 			ull n;
 			cin>>n;
-			SetColor(2,0); 
 			pf(n);
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="gcd"){
 			ull n,m;
 			cin>>n>>m;
-			SetColor(2,0); 
+			succ(); 
 			cout<<gcd(n,m)<<endl;
-			SetColor(15,0); 
+			SetColor(forgc,backc); 
 		}else if(code=="lcm"){
 			ull n,m;
 			cin>>n>>m;
-			SetColor(2,0);
+			succ();
 			cout<<lcm(n,m)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="+"){
 			long double n,m;
 			cin>>n>>m;
-			SetColor(2,0);
+			succ();
 			cout<<n<<'+'<<m<<'='<<n+m<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="-"){
 			long double n,m;
 			cin>>n>>m;
-			SetColor(2,0);
+			succ();
 			cout<<n<<'-'<<m<<'='<<n-m<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="*"){
 			long double n,m;
 			cin>>n>>m;
-			SetColor(2,0);
+			succ();
 			cout<<n<<"×"<<m<<'='<<n*m<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="/"){
 			long double n,m;
 			cin>>n>>m;
-			SetColor(2,0);
+			succ();
 			cout<<n<<"÷"<<m<<'='<<n/m<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="^"){
 			long double n,m;
 			cin>>n>>m;
-			SetColor(2,0);
+			succ();
 			cout<<n<<'^'<<m<<'='<<pow(n,m)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="ispn"){
 			ull n;
 			cin>>n;
-			SetColor(2,0);
-			if(ispn(n)) cout<<"是。\n"; else cout<<"否。\n";
-			SetColor(15,0);
+			succ();
+			switch(lang){
+				case 1:
+					if(ispn(n)) cout<<"是。\n"; else cout<<"否。\n";
+					break;
+				case 2:
+					if(ispn(n)) cout<<"True.\n"; else cout<<"False.\n";
+					break;
+			}
+			
+			SetColor(forgc,backc);
 		}else if(code=="cb"){
-			SetColor(2,0);
 			cb();
 			cout<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="stp"){
 			cin>>stp;
 			stp=min(900,stp);
-			SetColor(2,0);
-			cout<<setprecision(stp)<<"已将精度设为"<<stp<<endl;
-			SetColor(15,0);
+			succ();
+			cout<<setprecision(stp);
+			switch(lang){
+				case 1:
+					cout<<"已将精度设为";
+					break;
+				case 2:
+					cout<<"Precision has been set to ";
+					break;
+			}
+			cout<<stp<<endl;
+			SetColor(forgc,backc);
 		}else if(code=="dis"){
 			long double x1,y1,x2,y2;
 			cin>>x1>>y1>>x2>>y2;
-			SetColor(2,0);
+			succ();
 			cout<<pow(pow((x2-x1),2)+pow((y2-y1),2),0.5)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dsin"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"sin "<<d<<"°="<<sin(d/rtd)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dcos"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"cos "<<d<<"°="<<cos(d/rtd)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dtan"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"tan "<<d<<"°="<<tan(d/rtd)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dcot"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"cot "<<d<<"°="<<1.0/tan(d/rtd)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dsec"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"sec "<<d<<"°="<<1.0/cos(d/rtd)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dcsc"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"csc "<<d<<"°="<<1.0/sin(d/rtd)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="rsin"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"sin "<<d<<"="<<sin(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="rcos"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"cos "<<d<<"="<<cos(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="rtan"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"tan "<<d<<"="<<tan(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="rcot"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"cot "<<d<<"="<<1.0/tan(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="rsec"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"sec "<<d<<"="<<1.0/cos(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="rcsc"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"csc "<<d<<"="<<1.0/sin(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dasin"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arcsin "<<d<<"="<<asin(d)*rtd<<"°\n";
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dacos"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arccos "<<d<<"="<<acos(d)*rtd<<"°\n";
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="datan"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arctan "<<d<<"="<<atan(d)*rtd<<"°\n";
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dacot"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arccot "<<d<<"="<<1.0/atan(d)*rtd<<"°\n";
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dasec"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arcsec "<<d<<"="<<1.0/acos(d)*rtd<<"°\n";
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="dacsc"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arccsc "<<d<<"="<<1.0/asin(d)*rtd<<"°\n";
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="rasin"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arcsin "<<d<<"="<<asin(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="racos"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arccos "<<d<<"="<<acos(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="ratan"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arctan "<<d<<"="<<atan(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="racot"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arccot "<<d<<"="<<1.0/atan(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="rasec"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arcsec "<<d<<"="<<1.0/acos(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="racsc"){
 			long double d;
 			cin>>d;
-			SetColor(2,0);
+			succ();
 			cout<<"arccsc "<<d<<"="<<1.0/asin(d)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="log"){
 			long double b,n;
 			cin>>b>>n;
-			SetColor(2,0);
+			succ();
 			cout<<"log "<<b<<'('<<n<<")="<<log(n)/log(b)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
+		}else if(code=="lg"){
+			long double n;
+			cin>>n;
+			succ();
+			cout<<"lg "<<n<<"="<<log(n)/log(10)<<endl;
+			SetColor(forgc,backc);
 		}else if(code=="ln"){
 			long double n;
 			cin>>n;
-			SetColor(2,0);
+			succ();
 			cout<<"ln "<<n<<"="<<log(n)/log(e)<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
 		}else if(code=="pi"){
-			SetColor(2,0);
+			succ();
 			cout<<3.14159265358979323846264338327950288<<endl;
-			SetColor(15,0);
+			SetColor(forgc,backc);
+		}else if(code=="cl"){
+			cout<<"1. 简体中文\n2. English\n";
+			cin>>lang;
+			succ();
+			switch(lang){
+				case 1:
+					cout<<"切换成功。\n";
+					break;
+				case 2:
+					cout<<"Switching successful.\n";
+					break;
+			}
+				SetColor(forgc,backc);
+		}else if(code=="cc"){
+			int lforgc=forgc,lbackc=backc;
+			cin>>forgc>>backc;
+			if(lforgc==forgc&&lbackc==backc){
+				prom();
+				switch(lang){
+					case 1:
+						cout<<"现颜色与改变前相同。\n";
+						break;
+					case 2:
+						cout<<"The current color is the same as before the change.\n";
+						break;
+				}
+			}
+			succ();
+			SetColor(forgc,backc);
+			cout<<"测试test1234567890.abcdefghijklmnopqrstuvwxyz\n";
 		}else{
 			SetColor(4,0);
-			cout<<"无效代码！\n";
-			SetColor(15,0);
+			switch(lang){
+				case 1:
+					cout<<"无效代码。\n";
+					break;
+				case 2:
+					cout<<"Invalid code.\n";
+					break;
+			}
+			SetColor(forgc,backc);
 		}
 	}return 0;
 }
